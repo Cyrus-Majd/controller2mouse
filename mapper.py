4ab104a58a03ce9340b30ae6c0f13a9a8632239d
Cyrus Majd <cyrusmajd2001@gmail.com>
2021-05-12 13:32:48 -0400

    multiprocessing progress

--- mapper.py
+++ mapper.py
@@ -15,6 +15,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+    # start multiprocessing
+
     # for device in devices:
     #     print(devices)
 
