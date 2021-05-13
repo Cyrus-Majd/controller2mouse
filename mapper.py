fe55079f5b480f8c65faf4d071819b736ca0d114
Cyrus Majd <cyrusmajd2001@gmail.com>
2021-05-13 03:17:08 -0400

    ok multithreading wont work, back to multiprocessing

--- mapper.py
+++ mapper.py
@@ -4,8 +4,6 @@ from inputs import get_gamepad
 from threading import Thread
 from time import sleep
 import pyautogui
-import queue
-import time
 import numpy as np
 
 
@@ -14,77 +12,7 @@ import numpy as np
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-class ThreadedInputs:
-    NOMATCH = 'No Match'
-
-    def __init__(self):
-        # Initialise gamepad command dictionary.
-        # Add gamepad commands using the append method before executing the start method.
-        self.gamepadInputs = {}
-        self.lastEventCode = self.NOMATCH
-        # Initialise the thread status flag
-        self.stopped = False
-        self.q = queue.LifoQueue()
-
-    def start(self):
-        # Start the thread to poll gamepad event updates
-        t = Thread(target=self.gamepad_update, args=())
-        t.daemon = True
-        t.start()
-
-    def gamepad_update(self):
-        while True:
-            # Should the thread exit?
-            if self.stopped:
-                return
-            # Code execution stops at the following line until a gamepad event occurs.
-            events = get_gamepad()
-            for event in events:
-                event_test = self.gamepadInputs.get(event.code, self.NOMATCH)
-                if event_test != self.NOMATCH:
-                    self.gamepadInputs[event.code] = event.state
-                    self.lastEventCode = event.code
-                    self.q.put(event.code)
-
-    def read(self):
-        # Return the latest command from gamepad event
-        if not self.q.empty():
-            newCommand = self.q.get()
-            while not self.q.empty():
-                trashBin = self.q.get()
-
-            return newCommand, self.gamepadInputs[newCommand]
-        else:
-            return self.NOMATCH, 0
-
-    def stop(self):
-        # Stop the game pad thread
-        self.stopped = True
-
-    def append_command(self, newCommand, newValue):
-        # Add new controller command to the list
-        if newCommand not in self.gamepadInputs:
-            self.gamepadInputs[newCommand] = newValue
-        else:
-            print('New command already exists')
-
-    def delete_command(self, commandKey):
-        # Remove controller command from list
-        if commandKey in self.gamepadInputs:
-            del self.gamepadInputs[commandKey]
-        else:
-            print('No command to delete')
-
-    def command_value(self, commandKey):
-        # Get command value
-        if commandKey in self.gamepadInputs:
-            return self.gamepadInputs[commandKey]
-        else:
-            return None
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi():
 
     # start multiprocessing
 
@@ -104,11 +32,6 @@ def print_hi(name):
     xSensitivity = 50;
     ySensitivity = 50;
 
-    gamepad = ThreadedInputs()
-
-    for input in gamepadInputs:
-        gamepad.append_command(input, gamepadInputs[input])
-
     while 1:
         events = get_gamepad()
         for event in events:
@@ -125,7 +48,7 @@ def print_hi(name):
                     print("\tVERTICAL MOVEMENT")
 
 def main():
-    print_hi("ijwauhkddhasjkhdjkas")
+    print_hi()
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
