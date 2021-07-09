ee7450021bed67560b138e147a99e652b9c8b341
Cyrus Majd <cyrusmajd2001@gmail.com>
2021-07-08 20:16:08 -0400

    multithreaded controller inputs

--- mapper.py
+++ mapper.py
@@ -1,63 +1,75 @@
 
 from inputs import devices
 from inputs import get_gamepad
-from threading import Thread
 from time import sleep
+import threading
 import pyautogui
 import numpy as np
-import pandas as pd
-
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-def print_hi():
-
-    # start multiprocessing
-    #in this thread
-
-    # for device in devices:
-    #     print(devices)
-    #bruh you know what i mean
+import pandas as pds
 
 
 
-    #printing some random stuff?
-    print(devices.gamepads)
-    print(devices.keyboards)
-    print(devices.mice)
-    print(devices.other_devices)
+## Threaded class.
+class Controller(threading.Thread):
 
-    #random deadzone that i just came up with. if the value is within this radius of zero, dont do anything.
-    deadzone = 7000;
+    # Constructor.
+    def __init__(self, xSensitivity, ySensitivity, deadzone):
+        self.xMotion = 0
+        self.yMotion = 0
+        self.outOfDeadzone = False
 
-    #mouse sentivity
-    xSensitivity = 50;
-    ySensitivity = 50;
+        self.livingFlag = True
+        self.xSensitivity = xSensitivity
+        self.ySensitivity = ySensitivity
+        self.deadzone = deadzone
+        threading.Thread.__init__(self)
+        self.threadLock = threading.Lock()
 
-    while 1:
-        events = get_gamepad()
-        for event in events:
-            #event.ev_type, event.code, event.state
-            # print(event.code, event.state)
-            if (event.state >= deadzone or event.state <= -deadzone):
-                if (event.code == "ABS_X"):
-                    print(event.code, event.state)
-                    pyautogui.moveRel(np.sign(int(event.state)) * 50, 0, duration=0)
-                    print("\tHORIZONTAL MOVEMENT")
-                if (event.code == "ABS_Y"):
-                    print(event.code, event.state)
-                    pyautogui.moveRel(0, -np.sign(int(event.state)) * 50, duration=0)
-                    print("\tVERTICAL MOVEMENT")
+    # Method to read in inputs from controller.
+    def readInputs(self):
+        # DEBUG: Connected devices.
+        for device in devices:
+            print(devices)
 
+        # DEBUG: Device details.
+        print(devices.gamepads)
+        print(devices.keyboards)
+        print(devices.mice)
+        print(devices.other_devices)
+
+        while (self.livingFlag):
+            events = get_gamepad()
+            for event in events:
+                if (event.state >= self.deadzone or event.state <= -self.deadzone):
+                    self.outOfDeadzone = True
+                    if (event.code == "ABS_X"):
+                        print(event.code, event.state)
+                        # pyautogui.moveRel(np.sign(int(event.state)) * self.xSensitivity, 0, duration=0)
+                        print("\tHORIZONTAL MOVEMENT")
+                    if (event.code == "ABS_Y"):
+                        print(event.code, event.state)
+                        # pyautogui.moveRel(0, -np.sign(int(event.state)) * self.ySensitivity, duration=0)
+                        print("\tVERTICAL MOVEMENT")
+                    self.outOfDeadzone = False
+    
+    # Default threading entry method.
+    def run(self):
+        self.readInputs()
+
+# Main method.
 def main():
-    print_hi()
+    obj = Controller(10, 10, 7000)
+    obj.start()
 
-# Press the green button in the gutter to run the script.
+    # Loop which checks for program ending flag AND updates mouse state/etc from inputs.
+    while(obj.livingFlag):
+
+        
+
+        
+
+    obj.join()
+
 if __name__ == "__main__":
 	""" This is executed when run from the command line """
 	main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
