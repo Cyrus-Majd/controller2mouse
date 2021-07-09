103808d9d7f8c10aa73633f19f5ce0390c112cd3
Cyrus Majd <cyrusmajd2001@gmail.com>
2021-07-09 13:57:47 -0400

    works real well, just a little bit choppy

--- mapper.py
+++ mapper.py
@@ -1,11 +1,13 @@
-
+from __future__ import division
 from inputs import devices
 from inputs import get_gamepad
 from time import sleep
+import math
 import threading
 import pyautogui
 import numpy as np
 import pandas as pds
+import autopy
 
 ## Thread to check if user wants to quit program.
 class ProgramManager(threading.Thread):
@@ -32,7 +34,8 @@ class Controller(threading.Thread):
     def __init__(self, xSensitivity, ySensitivity, deadzone):
         self.xMotion = 0
         self.yMotion = 0
-        self.outOfDeadzone = False
+        self.xAmplitude = 0
+        self.yAmplitude = 0
 
         self.livingFlag = True
         self.xSensitivity = xSensitivity
@@ -56,25 +59,58 @@ class Controller(threading.Thread):
         while (self.livingFlag):
             events = get_gamepad()
             for event in events:
-                if (event.state >= self.deadzone or event.state <= -self.deadzone):
-                    self.outOfDeadzone = True
+                if (event.state >= self.deadzone or event.state <= -self.deadzone):     # beyond deadzone
+                    ## MIN/MAX X VALUE: -32768 --> 32767
+                    ## MIN/MAX Y VALUE: 32767 --> -32768
                     if (event.code == "ABS_X"):
-                        print(event.code, event.state)
-                        pyautogui.moveRel(np.sign(int(event.state)) * self.xSensitivity, 0, duration=0)
-                        print("\tHORIZONTAL MOVEMENT")
+                        # print(event.code, event.state, math.fabs(int(event.state) / 32767))
+                        self.xMotion = np.sign(int(event.state)) * self.xSensitivity
+                        self.xAmplitude = math.fabs(int(event.state) / 32768)
+                        # print("\tHORIZONTAL MOVEMENT, AMP:", self.xAmplitude)
                     if (event.code == "ABS_Y"):
-                        print(event.code, event.state)
-                        pyautogui.moveRel(0, -np.sign(int(event.state)) * self.ySensitivity, duration=0)
-                        print("\tVERTICAL MOVEMENT")
-                    self.outOfDeadzone = False
+                        # print(event.code, event.state, math.fabs(int(event.state) / 32767))
+                        self.yMotion = -np.sign(int(event.state)) * self.ySensitivity
+                        self.yAmplitude = math.fabs(int(event.state) / 32768)
+                        # print("\tVERTICAL MOVEMENT, AMP:", self.yAmplitude)
+                # elif (event.state <= self.deadzone and event.state >= -self.deadzone):    # within deadzone
+                    # self.xAmplitude = 0
+                    # self.yAmplitude = 0
+                #     print("IN CENTER, AMPS:", self.xAmplitude, self.yAmplitude)
+                # else:
+                #     print("NO MOVEMENT, AMPS:", self.xAmplitude, self.yAmplitude)
     
     # Default threading entry method.
     def run(self):
         self.readInputs()
 
+def moveCursor(xOne, xTwo, yOne, yTwo):
+    pyautogui.PAUSE = 0.01
+    pyautogui.MINIMUM_DURATION = 0.1
+    pyautogui.moveTo(xTwo, yTwo, duration = .2, tween = pyautogui.linear)
+
+def calculatePath(xMotion, yMotion, xAmp, yAmp):
+    xPosition = pyautogui.position()[0]
+    yPosition = pyautogui.position()[1]
+
+    xDisplacement = int(xMotion * xAmp)
+    yDisplacement = int(yMotion * yAmp)
+
+    xDestination = xPosition + xDisplacement
+    yDestination = yPosition + yDisplacement
+
+    sleep(1)
+
+    print(xPosition, yPosition, xDestination, yDestination)
+
+    moveCursor(xPosition, xDestination, yPosition, yDestination)
+
 # Main method.
 def main():
-    controllerObj = Controller(10, 10, 7000)
+    pyautogui.FAILSAFE = False
+
+    # calculatePath(100, -100, 3, 3)
+
+    controllerObj = Controller(100, 100, 0)
     controllerObj.start()
 
     sleep(1)
@@ -84,7 +120,11 @@ def main():
 
     # Loop updates inputs from controller as long as the quit flag has not been raised.
     while(programManagerObj.quitFlag == False):
-        sleep(1)
+        if (controllerObj.xAmplitude > .05 or controllerObj.yAmplitude > .05):
+            # print("\tXMOTION: ", controllerObj.xMotion, "YMOTION:", controllerObj.yMotion, "AMPx:", controllerObj.xAmplitude, "AMPy:", controllerObj.yAmplitude)
+            # moveCursor(calculatePath(controllerObj.xMotion, controllerObj.yMotion, controllerObj.xAmplitude, controllerObj.yAmplitude))
+            pyautogui.moveRel(controllerObj.xMotion * controllerObj.xAmplitude, controllerObj.yMotion * controllerObj.yAmplitude, duration=.1)  ## MOVE X VALUE
+            # pyautogui.moveRel(0, controllerObj.yMotion * controllerObj.yAmplitude, duration=.15) ## MOVE Y VALUE
     
     controllerObj.livingFlag = False
     controllerObj.join()
